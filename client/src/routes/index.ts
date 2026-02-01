729f4f4fc5432e3c26c971e632c20e4a48578194
ASG Kali <dev.vivek16@gmail.com>
2026-02-02 00:04:09 +0530

    auth API done !

---
+++ client/src/routes/index.ts
@@ -0,0 +1,9 @@
+// import { authRoutes } from "./auth.routes";
+// import { dashboardRoutes } from "./dashboard.routes";
+// import { adminRoutes } from "./admin.routes";
+
+export const appRoutes = [
+//   ...authRoutes,
+//   ...dashboardRoutes,
+//   ...adminRoutes,
+];
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,4 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
 });
