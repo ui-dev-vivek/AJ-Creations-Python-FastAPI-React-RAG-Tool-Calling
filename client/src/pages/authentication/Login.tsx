a7c5bc15184e5281e1a30db8d6029f4a7a5137f9
ASG Kali <dev.vivek16@gmail.com>
2026-02-08 08:56:19 +0530

    authnecate

--- client/src/pages/authentication/Login.tsx
+++ client/src/pages/authentication/Login.tsx
@@ -1,6 +1,43 @@
+import api from "@/utils/api";
 import { Input } from "@heroui/input";
+import React from "react";
 
 const Login: React.FC = () => {
+
+    const [mobileNumber, setMobileNumber] = React.useState<string>("");
+    const [error, setError] = React.useState<string>("");
+    const [loading, setLoading] = React.useState<boolean>(false);
+    const [otpSent, setOtpSent] = React.useState<boolean>(false);
+    const [otp, setOtp] = React.useState<string>("");
+    const [resendTimer, setResendTimer] = React.useState<number>(0);
+    const [userExists, setUserExists] = React.useState<boolean | null>(null);
+    const [termsAccepted, setTermsAccepted] = React.useState<boolean>(false);
+
+    const handleMobileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setMobileNumber(e.target.value);
+    }
+    
+    const handleLogin = async () => {
+        setLoading(true);
+        setError("");
+        try {
+            const response = await api.post("/auth/authenticate", { mobile: mobileNumber });
+            console.log(response);
+            setOtpSent(true);
+        } catch (err) {
+            setError("Failed to send OTP. Please try again.");
+            console.error(err);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleVerifyOtp = () => {
+        const response = api.post("/auth/authenticate", { mobile: mobileNumber, otp: otp });
+        alert(response.token);
+    };
+
+
     return (
         <div className="min-h-screen bg-[##fdebf0] ">
             {/* 3 Equal Parts Layout - Responsive */}
@@ -23,14 +60,18 @@ const Login: React.FC = () => {
                                     labelPlacement="outside"
                                     placeholder="Enter your phone number"
                                     type="text"
+                                    value={mobileNumber || ""}
+                                    onChange={handleMobileChange}
                                 />
 
                             </div>
-                            <button className="mt-6  w-full bg-primary text-white rounded-md py-4 text-xl hover:bg-primary-dark transition">
+                            <button 
+                                onClick={handleLogin}
+                            className="mt-6  w-full bg-primary text-white rounded-md py-4 text-xl hover:bg-primary-dark transition">
                                 Continue
                             </button>
                         </div>
-                       <div className="px-6 md:px-8 py-5 bg-gradient-to-r from-default-50 to-default-100 border-t border-default-200">
+                       <div className="px-6 md:px-8 py-5 bg-gradient-to-r rounded-b-xl from-default-50 to-default-100 border-t border-default-200">
                             <p className="text-xs md:text-xs text-center text-default-600 leading-relaxed">
                                 By continuing, you agree to MAVA's&nbsp;
                                 <a className="underline text-primary font-semibold hover:text-primary/80 transition" href="#">
